0ce77067bc77e387fa369cc3353b12514e32792f
Luke Matakitoga <luke.matakitoga@cetsat.com>
2024-01-03 11:05:14 +0000

    More stuff

--- TheWarehouse/Data/Models/Supplier.cs
+++ TheWarehouse/Data/Models/Supplier.cs
@@ -5,6 +5,11 @@
         public int Id { get; set; }
         public string? Name { get; set; }
 
+        public string? AddressLine1 { get; set; }
+        public string? AddressLine2 { get; set; }
+        public string? City { get; set; }
+        public string? Postcode { get; set; }
+
         public List<Product>? Products { get; set; } = new();
     }
 }
---
+++ TheWarehouse/Migrations/20240103102132_SupplierAddress.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TheWarehouse.Migrations
+{
+    /// <inheritdoc />
+    public partial class SupplierAddress : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AddressLine1",
+                table: "Suppliers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "AddressLine2",
+                table: "Suppliers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "Suppliers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Postcode",
+                table: "Suppliers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AddressLine1",
+                table: "Suppliers");
+
+            migrationBuilder.DropColumn(
+                name: "AddressLine2",
+                table: "Suppliers");
+
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "Suppliers");
+
+            migrationBuilder.DropColumn(
+                name: "Postcode",
+                table: "Suppliers");
+        }
+    }
+}
